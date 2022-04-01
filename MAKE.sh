4b409c89c266fd887f94cf7cf1c3dafda72a4b59
David Allex <allexwin50@gmail.com>
2022-03-31 23:01:48 -0300

    Update MAKE.sh

--- MAKE.sh
+++ MAKE.sh
@@ -21,9 +21,9 @@ echo "#  [2] Recriar imagens de sistema (system, system_ext, product e vendor) #
 echo "#  [3] Remover bloatwares                                                #"
 echo "#  [4] Montar e desmontar partições                                      #"
 echo "#  [5] Compilar super partição                                           #"
-echo "#  [6] Sincronizar mods.img com a nova imagem                            #"
-echo "#  [7] Limpar residuos ao concluir todo o processo                       #"
-echo "#  [8] Sair                                                              #"
+#echo "#  [6] Sincronizar mods.img com a nova imagem                            #"
+echo "#  [6] Limpar residuos ao concluir todo o processo                       #"
+echo "#  [7] Sair                                                              #"
 echo "#========================================================================#"
 echo "# Digite a opcao desejada:                                               #"
 echo "#========================================================================#"
@@ -38,8 +38,8 @@ case "$x" in
 3) clear && chmod a+x scripts/03.sh && ./scripts/03.sh ;;
 4) clear && chmod a+x scripts/04.sh && ./scripts/04.sh ;;
 5) clear && chmod a+x scripts/05.sh && ./scripts/05.sh ;;
-6) clear && chmod a+x scripts/06.sh && ./scripts/06.sh ;;
-7) clear && chmod a+x scripts/clear.sh && ./scripts/clear.sh ;;
-8) clear && echo "Saindo..." && clear; exit;;
+#6) clear && chmod a+x scripts/06.sh && ./scripts/06.sh ;;
+6) clear && chmod a+x scripts/clear.sh && ./scripts/clear.sh ;;
+7) clear && echo "Saindo..." && clear; exit;;
 *) clear && echo "Opção inválida!"; esac done } && menu
 
