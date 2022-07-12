ec13bab9f414e40f1bda5c35cca5882cadd0d47a
David Allex <allexwin50@gmail.com>
2022-07-12 15:12:46 -0300

    Update optimize_system.sh

--- scripts/optimize_system.sh
+++ scripts/optimize_system.sh
@@ -1,5 +1,5 @@
 [ ! -e "system_new.img" ] && echo "Esta faltando o arquivo system_new.img para que possamos prosseguir..." && exit
-umount system_new && sudo rm -rf system_new
+umount system_new && umount system_new && sudo rm -rf system_new
 e2fsck -yf system_new.img
 resize2fs -M system_new.img
 e2fsck -yf system_new.img
