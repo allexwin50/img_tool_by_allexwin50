301748a35e30edc17566a315b59b5a244f47d5ee
David Allex <allexwin50@gmail.com>
2022-07-12 12:23:40 -0300

    Add files via upload

--- MAKE.sh
+++ MAKE.sh
@@ -5,6 +5,46 @@ chmod a+x lpmake
 chmod a+x lpunpack
 chmod a+x simg2img
 
+chmod a+x scripts/01.sh
+chmod a+x scripts/02.sh
+chmod a+x scripts/03.sh
+chmod a+x scripts/04.sh
+chmod a+x scripts/05.sh
+chmod a+x scripts/06.sh
+chmod a+x scripts/bloatwares_pe_plus.sh
+chmod a+x scripts/bloatwares_pe.sh
+chmod a+x scripts/bloatwares_stock_lite.sh
+chmod a+x scripts/clear.sh
+chmod a+x scripts/create_oem_new.sh
+chmod a+x scripts/create_product_gsi.sh
+chmod a+x scripts/create_product_new.sh
+chmod a+x scripts/create_system_ext_gsi.sh
+chmod a+x scripts/create_system_ext_new.sh
+chmod a+x scripts/create_system_gsi.sh
+chmod a+x scripts/create_system_new.sh
+chmod a+x scripts/create_vendor_new.sh
+chmod a+x scripts/extractor.sh
+chmod a+x scripts/extract_stock_zip.sh
+chmod a+x scripts/Firmware_extractor-master.zip
+chmod a+x scripts/optimize_oem.sh
+chmod a+x scripts/optimize_product.sh
+chmod a+x scripts/optimize_system_ext.sh
+chmod a+x scripts/optimize_system.sh
+chmod a+x scripts/optimize_vendor.sh
+chmod a+x scripts/raw_oem.sh
+chmod a+x scripts/raw_super.sh
+chmod a+x scripts/raw_system.sh
+chmod a+x scripts/repack_super_ab_com_system_ext.sh
+chmod a+x scripts/repack_super_ab.sh
+chmod a+x scripts/repack_super_ab_to_a_com_system_ext_gsi.sh
+chmod a+x scripts/repack_super_ab_to_a_com_system_ext.sh
+chmod a+x scripts/repack_super_ab_to_a_gsi.sh
+chmod a+x scripts/repack_super_ab_to_a.sh
+chmod a+x scripts/sincronize_system_product.sh
+chmod a+x scripts/sincronize_system.sh
+chmod a+x scripts/sincronize_system_system_ext_product.sh
+chmod a+x scripts/unpack_super.sh
+
 x="teste"
 menu ()
 {
@@ -33,13 +73,13 @@ echo "#========================================================================#
 
 case "$x" in
 
-1) clear && chmod a+x scripts/01.sh && ./scripts/01.sh ;;
-2) clear && chmod a+x scripts/02.sh && ./scripts/02.sh ;;
-3) clear && chmod a+x scripts/03.sh && ./scripts/03.sh ;;
-4) clear && chmod a+x scripts/04.sh && ./scripts/04.sh ;;
-5) clear && chmod a+x scripts/05.sh && ./scripts/05.sh ;;
+1) clear && ./scripts/01.sh ;;
+2) clear && ./scripts/02.sh ;;
+3) clear && ./scripts/03.sh ;;
+4) clear && ./scripts/04.sh ;;
+5) clear && ./scripts/05.sh ;;
 #6) clear && chmod a+x scripts/06.sh && ./scripts/06.sh ;;
-6) clear && chmod a+x scripts/clear.sh && ./scripts/clear.sh ;;
+6) clear && ./scripts/clear.sh ;;
 7) clear && echo "Saindo..." && clear; exit;;
 *) clear && echo "Opção inválida!"; esac done } && menu
 
