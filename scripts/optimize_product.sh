256a8aec47aa39cfd27bd0c247f6ec9d821f366f
David Allex <allexwin50@gmail.com>
2022-07-12 15:11:15 -0300

    Update optimize_product.sh

--- scripts/optimize_product.sh
+++ scripts/optimize_product.sh
@@ -1,5 +1,5 @@
 [ ! -e "product_new.img" ] && echo "Esta faltando o arquivo product_new.img para que possamos prosseguir..." && exit
-umount product_new && sudo rm -rf product_new
+umount product_new && umount product_new && sudo rm -rf product_new
 e2fsck -yf product_new.img
 resize2fs -M product_new.img
 e2fsck -yf product_new.img
