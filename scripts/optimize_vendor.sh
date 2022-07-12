641de94136c8f3137e29bed0959cec2ae8f8e3e6
David Allex <allexwin50@gmail.com>
2022-07-12 13:17:53 -0300

    Add files via upload

--- scripts/optimize_vendor.sh
+++ scripts/optimize_vendor.sh
@@ -1,4 +1,5 @@
 [ ! -e "vendor_new.img" ] && echo "Esta faltando o arquivo vendor_new.img para que possamos prosseguir..." && exit
+umount vendor_new && sudo rm -rf vendor_new
 e2fsck -yf vendor_new.img
 resize2fs -M vendor_new.img
 e2fsck -yf vendor_new.img
