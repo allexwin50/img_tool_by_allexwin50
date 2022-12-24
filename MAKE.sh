561bf9b2a3e3c3ed75d25d3e546ddc7ceb409fd1
David Allex <allexwin50@gmail.com>
2022-12-24 19:31:12 -0300

    Update MAKE.sh

--- MAKE.sh
+++ MAKE.sh
@@ -1,47 +1,7 @@
 #!/usr/bin/env bash
 
-chmod a+x MAKE.sh
-chmod a+x lpmake
-chmod a+x lpunpack
-chmod a+x simg2img
-
-chmod a+x scripts/01.sh
-chmod a+x scripts/02.sh
-chmod a+x scripts/03.sh
-chmod a+x scripts/04.sh
-chmod a+x scripts/05.sh
-chmod a+x scripts/06.sh
-chmod a+x scripts/bloatwares_stock_lite.sh
-chmod a+x scripts/clear.sh
-chmod a+x scripts/create_oem_new.sh
-chmod a+x scripts/create_product_gsi.sh
-chmod a+x scripts/create_product_new.sh
-chmod a+x scripts/create_system_ext_gsi.sh
-chmod a+x scripts/create_system_ext_new.sh
-chmod a+x scripts/create_system_gsi.sh
-chmod a+x scripts/create_system_new.sh
-chmod a+x scripts/create_vendor_new.sh
-chmod a+x scripts/extractor.sh
-chmod a+x scripts/extract_stock_zip.sh
-chmod a+x scripts/Firmware_extractor-master.zip
-chmod a+x scripts/optimize_oem.sh
-chmod a+x scripts/optimize_product.sh
-chmod a+x scripts/optimize_system_ext.sh
-chmod a+x scripts/optimize_system.sh
-chmod a+x scripts/optimize_vendor.sh
-chmod a+x scripts/raw_oem.sh
-chmod a+x scripts/raw_super.sh
-chmod a+x scripts/raw_system.sh
-chmod a+x scripts/repack_super_ab_com_system_ext.sh
-chmod a+x scripts/repack_super_ab.sh
-chmod a+x scripts/repack_super_ab_to_a_com_system_ext_gsi.sh
-chmod a+x scripts/repack_super_ab_to_a_com_system_ext.sh
-chmod a+x scripts/repack_super_ab_to_a_gsi.sh
-chmod a+x scripts/repack_super_ab_to_a.sh
-chmod a+x scripts/sincronize_system_product.sh
-chmod a+x scripts/sincronize_system.sh
-chmod a+x scripts/sincronize_system_system_ext_product.sh
-chmod a+x scripts/unpack_super.sh
+chmod a+x *
+chmod a+x scripts/*
 
 x="teste"
 menu ()
