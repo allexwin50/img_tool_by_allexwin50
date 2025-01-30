690f0f5407c5eeb1dcf3e0e861270dcdd600c43b
David Allex <allexwin50@gmail.com>
2025-01-30 19:55:01 -0300

    Add files via upload

--- scripts/06.sh
+++ scripts/06.sh
@@ -11,10 +11,11 @@ echo "#================================================================#"
 echo "#  [1] Mods.img para system.img                                  #"
 echo "#  [2] Mods.img para system.img e product.img                    #"
 echo "#  [3] Mods.img para system.img, system_ext.img e product.img    #"
-echo "#  [4] mods.img para samsung                                     #"
+echo "#  [4] mods.img para samsung M62                                 #"
+echo "#  [5] mods.img para samsung F62                                 #"
 echo "#                       ------------------                       #"
-echo "#  [5] Voltar para o menu principal                              #"
-echo "#  [6] Sair                                                      #"
+echo "#  [6] Voltar para o menu principal                              #"
+echo "#  [7] Sair                                                      #"
 echo "#================================================================#"
 echo "# Digite a opcao desejada:                                       #"
 echo "#================================================================#"
@@ -27,8 +28,9 @@ case "$x" in
 1) echo "Iniciando processo, aguarde..."; ./scripts/sincronize_system.sh && echo "Concluído!" ;;
 2) echo "Iniciando processo, aguarde..."; ./scripts/sincronize_system_product.sh  && echo "Concluído!" ;;
 3) echo "Iniciando processo, aguarde..."; ./scripts/sincronize_system_system_ext_product.sh  && echo "Concluído!" ;;
-4) echo "Iniciando processo, aguarde..."; ./scripts/sincronize_samsung.sh  && echo "Concluído!" ;;
-5) clear && ./MAKE.sh ;;
-6) clear && echo "Saindo..." && clear; exit;;
+4) echo "Iniciando processo, aguarde..."; ./scripts/sincronize_samsung_m62.sh  && echo "Concluído!" ;;
+5) echo "Iniciando processo, aguarde..."; ./scripts/sincronize_samsung_f62.sh  && echo "Concluído!" ;;
+6) clear && ./MAKE.sh ;;
+7) clear && echo "Saindo..." && clear; exit;;
 *) clear && echo "Opção inválida!"; esac done } && menu
 
---
+++ scripts/sincronize_samsung_f62.sh
@@ -0,0 +1,80 @@
+[ ! -e "odm_new.img" ] && echo "Esta faltando o arquivo odm_new.img para que possamos prosseguir..." && exit
+[ ! -e "product_new.img" ] && echo "Esta faltando o arquivo product_new.img para que possamos prosseguir..." && exit
+[ ! -e "system_new.img" ] && echo "Esta faltando o arquivo system_new.img para que possamos prosseguir..." && exit
+[ ! -e "vendor_new.img" ] && echo "Esta faltando o arquivo vendor_new.img para que possamos prosseguir..." && exit
+[ ! -e "mods.img" ] && echo "Esta faltando o arquivo mods.img para que possamos prosseguir..." && exit
+sudo umount odm_new
+sudo rmdir odm_new
+sudo umount product_new
+sudo rmdir product_new
+sudo umount system_new
+sudo rmdir system_new
+sudo umount vendor_new
+sudo rmdir vendor_new
+e2fsck -yf odm_new.img
+fallocate -l 1024M odm_new.img
+resize2fs odm_new.img 1024M
+e2fsck -yf odm_new.img
+e2fsck -yf product_new.img
+fallocate -l 5000M product_new.img
+resize2fs product_new.img 5000M
+e2fsck -yf product_new.img
+e2fsck -yf system_new.img
+fallocate -l 7000M system_new.img
+resize2fs system_new.img 7000M
+e2fsck -yf system_new.img
+e2fsck -yf vendor_new.img
+fallocate -l 2048M vendor_new.img
+resize2fs vendor_new.img 2048M
+e2fsck -yf vendor_new.img
+mkdir mods
+mkdir optics
+mkdir odm_new
+mkdir product_new
+mkdir system_new
+mkdir vendor_new
+sudo mount -r mods.img mods
+sudo mount -rw optics.img optics
+sudo mount -rw odm_new.img odm_new
+sudo mount -rw product_new.img product_new
+sudo mount -rw system_new.img system_new
+sudo mount -rw vendor_new.img vendor_new
+sudo cp -r --preserve=all mods/optics/* optics/
+#sudo cp -r --preserve=all mods/odm/* odm_new/
+sudo cp -r --preserve=all mods/product/* product_new/
+sudo cp -r --preserve=all mods/system/* system_new/system/
+#sudo cp -r --preserve=all mods/only_f62_binary_7/framework/* system_new/system/framework
+sudo cp -r --preserve=all mods/vendor/* vendor_new/
+sudo cp -r --preserve=all mods/#placebo.img odm_new/
+sudo cp -r --preserve=all mods/#placebo2.img odm_new/
+sudo cp -r --preserve=all mods/#placebo.img product_new/
+sudo cp -r --preserve=all mods/#placebo2.img product_new/
+sudo cp -r --preserve=all mods/#placebo-x2.img system_new/system/
+sudo cp -r --preserve=all mods/#placebo.img vendor_new/
+sudo cp -r --preserve=all mods/#placebo2.img vendor_new/
+sudo umount mods
+sudo rmdir mods
+sudo umount optics
+sudo rmdir optics
+sudo umount odm_new
+sudo rmdir odm_new
+sudo umount product_new
+sudo rmdir product_new
+sudo umount system_new
+sudo rmdir system_new
+sudo umount vendor_new
+sudo rmdir vendor_new
+e2fsck -yf odm_new.img
+resize2fs -M odm_new.img
+e2fsck -yf odm_new.img
+e2fsck -yf product_new.img
+resize2fs -M product_new.img
+e2fsck -yf product_new.img
+e2fsck -yf system_new.img
+resize2fs -M system_new.img
+e2fsck -yf system_new.img
+e2fsck -yf vendor_new.img
+resize2fs -M vendor_new.img
+e2fsck -yf vendor_new.img
+echo "Sincronização concluída"
+
